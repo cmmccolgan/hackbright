358b01069ea55232015737a7e9b5ccd216dd98ed
Cameron McColgan <cmmccolgan@gmail.com>
2016-05-08 20:28:32 -0700

    Sunday scope test

--- homework/bart_kiosk_simulator.py
+++ homework/bart_kiosk_simulator.py
@@ -1,48 +1,51 @@
 '''
-PROJECT DESCRIPTION:
-We are going to simulate a BART kiosk. 
-BART has a smart card system called “Clipper” where riders can load money onto a smart card.
-The smart card will keep track of the amount of money the rider has for train rides. 
-When the card is used to pay for train rides and the cost of the ride is deducted. 
-The rider does not need to have exact change for each train ride.
-They can simply load a large sum of money onto the card and take as many rides as they want until the monetary amount on the card runs out.
+Homework assigment - bart_kiosk
 
-dollar_1 =[1,2,3,4]
-dollar_5 = [5]
-dollar_10 = [10]
-dollar_20 = [20]
-
-#FUNCTIONS
-def load_card(rider_balance,deposit):
-	
-	return 
-
-#Argument 1:
-print load_card(0,1.00)
-#Argument 2:
-print load_card(0,5.00)
-#Argument 3:
-print load_card(0,10.00)
-#Argument 4:
-print load_card(0,20.00)
 '''
-def changer(nominals, total):
-	#Length of list
-	used_nominals = [0] * len(nominals)
-	for i, n in enumerate(nominals[::-1]):
-		used_nominals[i] = total // n
-		total -= used_nominals[i]*n
-		return used_nominals, total
+#Functions Used: 
+def load_card(one_dollar_bills,five_dollar_bills,ten_dollar_bills,twenty_dollar_bills):
+	ones = one_dollar_bills * 1
+	fives = five_dollar_bills * 5
+	tens = ten_dollar_bills * 10
+	twenties = twenty_dollar_bills * 20
+	print "You entered %i $1 bills, %i $5 bills, %i $10 bills, and %i $20 bills" % (one_dollar_bills,five_dollar_bills,ten_dollar_bills,twenty_dollar_bills)
+	return float(ones + fives + tens + twenties)
 
-#def dict_changer(nominals, total): 
-#	dict_nominals_count = {n: 0 for n in nominals} for n in nominals[::-1]: 
-#	dict_nominals_count[n] = total // n total -= dict_nominals_count[n]*n 
-#	return dict_nominals_count, total
+def travel_to_destination(fare_price,card_value):
+	if card_value > fare_price:
+		print "Welcome aboard, enjoy your trip!"
+	else:
+		print "You need more money!"
 
-def coin_changer(total):
-  return changer([1,2],total)
+#MAIN PROGRAM ----------------------------------------------------------------------------
+def bart_kiosk():
+	
+	#Bart Travel Options:
+	DUBLIN_TO_POWELL = 6.15
+	FRUITVALE_TO_UNION_CITY = 3.80
+	ORINDA_TO_RICHMOND = 3.35
+	HAYWARD_TO_CONCORD = 5.20
+	FREMONT_TO_COLMA = 6.60
 
-def note_changer(total):
-  return changer([5,10,20],total)
+	print "Test Cases for Load Card"
+	print load_card(0,0,0,0)  
+	print load_card(0,0,0,9)
+	print load_card(2,3,0,0)
+	print load_card(3,1,1,3)
 
-nominal_counts, left = changer([1,2,5,10,20], total)
+	print "Test Cases Travel to Destination"
+	clipper_card1 = load_card(3,0,0,0)
+	travel_to_destination(FREMONT_TO_COLMA,clipper_card1)
+	
+	clipper_card2 = load_card(1,0,0,1)
+	travel_to_destination(HAYWARD_TO_CONCORD,clipper_card2)
+
+	clipper_card3 = load_card(1,1,0,0)
+	travel_to_destination(DUBLIN_TO_POWELL,clipper_card3)
+
+	clipper_card4 = load_card(2,0,0,0)	
+	travel_to_destination(FRUITVALE_TO_UNION_CITY,clipper_card4)
+
+#Required to run main program
+if __name__ == '__main__':
+	bart_kiosk()
