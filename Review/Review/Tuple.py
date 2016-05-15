002563b5883e7cdc87b88ae1209ddd9b937a2a42
Cameron McColgan <cmmccolgan@gmail.com>
2016-05-15 14:22:24 -0700

    Sunday review

---
+++ Review/Review/Tuple.py
@@ -0,0 +1,31 @@
+'''
+Review Tuples
+'''
+#EXERCISE1 ------------------------------------
+my_tuple = (1,2,"kitten",4,"five")
+my_tuples = (1,2,"kitten",4,"five",6,7)
+
+#1 
+print "1", my_tuple[2]
+#2
+print "2", my_tuple[3]
+#3
+print "3", my_tuple[1]
+#4a
+print "4", my_tuple[4:]
+#4b
+print "5", my_tuple[4]
+#4c 
+print "6", my_tuple[-1]
+#5 
+print "7", len(my_tuple)
+#6 
+#print "8", my_tuple = my_tuple +6
+#7 Table 
+print "Table 1", my_tuples[0]
+print "Table 2", my_tuples[2:3]
+print "Table 3", my_tuples[1:5]
+print "Table 4", my_tuples[1:10]
+print "Table 5", my_tuples[-1]
+print "Table 6", my_tuples[-1:-4]
+print "Table 7", my_tuples[-4:]
