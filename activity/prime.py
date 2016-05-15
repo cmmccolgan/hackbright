07b9e21f99941ad4b05998461ee4b8f50522331a
Cameron McColgan <cmmccolgan@gmail.com>
2016-05-15 13:48:11 -0700

    Sunday Review

--- activity/prime.py
+++ activity/prime.py
@@ -1,17 +1,43 @@
-# Python program to check if the input number is prime or not
+'''
+Python program to check if the input number is prime or not
+'''
 
-# variable - take input from the user
-num = int(raw_input("Enter a number: "))
+#USED FUNCITON
+def ask_number():
+	num = int(raw_input("Enter a number:"))
+	return num 
 
-# prime numbers are greater than 1
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           break
-   else:
-       print(num,"is a prime number")
+def is_prime(user_input):
+	if user_input == 0 or user_input == 1:
+		return False
+	for x in range(2, user_input):
+		if user_input % x == 0:
+			return False
+	else:
+		return True
 
-else:
-   print(num,"is not a prime number")
+def want_to_play():
+	wanna_input = raw_input("Want to play again?").lower()
+ 	if wanna_input == "yes" or "y":
+ 		ask_number
+ 	else:
+ 		print "Thanks for playing"
+
+#MAIN FUNCTION:
+def prime_checker():
+	#Ask the user for an integer input & Define as global variable
+	user_input = ask_number()
+	#Define users number prime status
+	check_prime = is_prime(user_input)
+
+	#Result for users input & Loop the user back
+	while (check_prime == True):
+		print "%i is prime!" %(user_input)
+		break
+	else:
+		print "Your number is not prime :("
+		want_to_play()
+
+#Required for main
+if __name__ == '__main__':
+	prime_checker()
