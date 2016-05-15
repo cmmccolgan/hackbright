48eb072591494b793f28f3eafd7505928549a474
Cameron McColgan <cmmccolgan@gmail.com>
2016-05-15 15:22:40 -0700

    Sunday Review

--- Review/Loops.py
+++ Review/Loops.py
@@ -12,17 +12,17 @@ Review Loop
 # print "8", range (5,1,-1)
 
 #EXERCISE 2 ----------------------------------
-#Understanding Range:
-#print range (1,21)
-#1
-#for index in range(1,21):
-#	print index,
-#2
+# #Understanding Range:
+# print range (1,21)
+# #1
 # for index in range(1,21):
-	# if (index == 13):
-	# 	print "Hello"
-	# else:
-	# 	print index,
+# 	print index,
+# #2
+# for index in range(1,21):
+# 	if (index == 13):
+# 		print "Hello"
+# 	else:
+# 		print index,
 
 #EXERCISE 3 -----------------------------------
 # fruits = "apples", "bananas", "oranges"
@@ -34,29 +34,10 @@ Review Loop
 # 	print fruits[i]
 
 #2
-# def sum_nums (num):
-# 	sum =0
-# 	for index in range (num):
-# 		sum+= index
-# 		return sum
+def sum_nums (num):
+	sum =0
+	for index in range (num):
+		sum+= index
+		return sum
 
-# print sum_nums(3)
-
-#EXERCISE 4 -----------------------------------
-def fizz_buzz(num):
-	#We want to count to 100 and print each index
-	for index in range(num):
-		#If number is divisible by 3 and 5 - print FIZZBUZZ
-		if index % 3 ==0 and index % 5 ==0:
-			print "FIZZBUZZ"
-		#If number is divisible by -  print FIZZ
-		elif index % 3 ==0:
-			print "FIZZ"
-		#If number is divisible by 5 - print BUZZ
-		elif index % 5 ==0:
-			print "BUZZ"
-		# print index
-		else:
-			print index
-	
-print fizz_buzz(101)
+print sum_nums(3)
